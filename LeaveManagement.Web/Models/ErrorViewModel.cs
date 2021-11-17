b3bb48f7164e34c98c3cd5b586468d8599c2278c
Trevoir Williams <trevoir.williams@gmail.com>
2021-11-17 11:30:51 -0500

    Add project files.

---
+++ LeaveManagement.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace LeaveManagement.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
