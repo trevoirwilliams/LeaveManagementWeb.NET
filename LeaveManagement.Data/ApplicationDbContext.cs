b974deeb643d14177ae77e0ac4fe9e9bebcc68b7
Trevoir Williams <trevoir.williams@gmail.com>
2022-04-13 21:06:27 -0500

    Added Context Auditing and fixed cancel requests. Disabled Nullable on all projects.

--- LeaveManagement.Data/ApplicationDbContext.cs
+++ LeaveManagement.Data/ApplicationDbContext.cs
@@ -19,6 +19,21 @@ namespace LeaveManagement.Data
             builder.ApplyConfiguration(new UserRoleSeedConfiguration());
         }
 
+        public override Task<int> SaveChangesAsync(CancellationToken cancellationToken = default)
+        {
+            foreach (var entry in base.ChangeTracker.Entries<BaseEntity>().Where(q => q.State == EntityState.Added || q.State == EntityState.Modified))
+            {
+                entry.Entity.DateModified = DateTime.Now;
+
+                if(entry.State == EntityState.Added)
+                {
+                    entry.Entity.DateCreated = DateTime.Now;
+                }
+            }
+
+            return base.SaveChangesAsync(cancellationToken);
+        }
+
         public DbSet<LeaveType> LeaveTypes { get; set; }
         public DbSet<LeaveAllocation> LeaveAllocations { get; set; }
         public DbSet<LeaveRequest> LeaveRequests { get; set; }
